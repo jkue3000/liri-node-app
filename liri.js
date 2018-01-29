c180401c24cc35ea3a7317873ce91d25461f5f8e
unknown <jkue3000@gmail.com>
2018-01-28 20:28:33 -0500

    Working on JS

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+// variable to access api's "NPM INSTALL"
+
+// spotify
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+// twitter
+var client = require('twitter');
+
+
+
+
+
