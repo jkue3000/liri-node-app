0558f89fc8aa0724bb1652c2f458b66a142544f6
unknown <jkue3000@gmail.com>
2018-01-31 17:17:58 -0500

    Not done HW

--- liri.js
+++ liri.js
@@ -1,15 +1,38 @@
-require("dotenv").config();
+// process argv Global
+var userInput = process.argv[];
 
-// variable to access api's "NPM INSTALL"
+// node package to write files
+var fs = require("fs");
 
+// array to use the for loop
+var inputArray = [];
+
+// var for user input to use in the userInput
+var userInput = process.argv[];
+
+// for loop to search process.arv
+for(i = 0; i < inputArray.length; i++){
+	
+}
+
+// DOTENV require call
+var getEnv = require("dotenv").config();
+
+// variables to access api's "NPM INSTALL"
 // spotify
-var Spotify = require('node-spotify-api');
+var Spotify = require(keys.spotify);
 var spotify = new Spotify(keys.spotify);
 
 // twitter
-var client = require('twitter');
+var Twitter = require('twitter');
 
+// OMBD movie request
+var request = require('request');
+request('http://www.omdbapi.com/?t='+ userInput +'&y=&plot=short&apikey=trilogy'), function(error, response, body){
 
+}
 
-
-
+// get API's to call back information
+// get OMBD API to call back
+// ????????make constructor object to be able to have base layout callback response????
+// ???????add indavidual properties to each api call???????
