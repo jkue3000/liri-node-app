a28c353f476e2f326ed0ab3cde9f2ed1dbb501ae
unknown <jkue3000@gmail.com>
2018-01-31 19:24:01 -0500

    Tokens need validation

--- liri.js
+++ liri.js
@@ -1,36 +1,52 @@
+// DOTENV require call
+require("dotenv").config();
+var requireKeys = require("./keys.js");
+
 // process argv Global
-var userInput = process.argv[];
+var userInput = process.argv;
+var userSearch = process.argv[2];
 
 // node package to write files
 var fs = require("fs");
 
-// array to use the for loop
-var inputArray = [];
-
-// var for user input to use in the userInput
-var userInput = process.argv[];
-
-// for loop to search process.arv
-for(i = 0; i < inputArray.length; i++){
-	
-}
-
-// DOTENV require call
-var getEnv = require("dotenv").config();
-
 // variables to access api's "NPM INSTALL"
 // spotify
-var Spotify = require(keys.spotify);
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require('node-spotify-api');
+// var spotify = new Spotify('keys.spotify');
 
 // twitter
 var Twitter = require('twitter');
+var client = new Twitter('requireKeys.twitter')
+var params = {screen_name: 'Kuespace', count: 20};
+
+if (userSearch === "my-tweets"){
+	// function to get the twitter response
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  		if (error) {
+    		console.log(error);
+  		}else{
+  			console.log(tweets);
+  		}
+	});
+}
+
+// if (userSearch === "spotify-this-song"){
+// 	// function to get the spotify response
+// 	spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   		if (err) {
+//     		return console.log('Error occurred: ' + err);
+//   		}
+// 		console.log(data); 
+// 	});
+
+// }
+
 
 // OMBD movie request
-var request = require('request');
-request('http://www.omdbapi.com/?t='+ userInput +'&y=&plot=short&apikey=trilogy'), function(error, response, body){
+// var request = require('request');
+// request('http://www.omdbapi.com/?t='+ userInput +'&y=&plot=short&apikey=trilogy'), function(error, response, body){
 
-}
+// }
 
 // get API's to call back information
 // get OMBD API to call back
